3a24d20954258fe2c4d0ad8730d2abb84a53c339
shubham maithani <shubhammaithani1@gmail.com>
2022-07-27 12:31:21 -0700

    Sum of three integer

--- functionalprogs.java
+++ functionalprogs.java
@@ -1,28 +1,38 @@
 package FunctionalProgram;
 import java.util.Scanner;
 public class functionalprogs {
+	
+
+	static void Sum(){
+        Scanner scr = new Scanner(System.in);
+        System.out.println("Enter the value of N:");
+        int N = scr.nextInt();
+        int[] A = new int[N];
+        int found=0;
+        int count =0;
+
+        for (int i=0;i<N;i++){
+            A[i] = scr.nextInt();
+        }
+        for (int i=0;i<N-2;i++){
+            for (int j=i+1;j<N-1;j++){
+                for (int k=j+1;k<N;k++){
+                    if ((A[i]+A[j]+A[k])==0){
+                        System.out.println(A[i] +" " +A[j]+" "+A[k]);
+                        found = 1;
+                        count++;
+                    }
+                }
+            }
+        }
+        System.out.println("Number of distinct triplets are: "+ count);
+        if(found == 0){
+            System.out.println("not found");
+        }
+	}
 
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number of rows : ");
-        int rows = sc.nextInt();
-        System.out.print("Enter the number of columns : ");
-        int columns = sc.nextInt();
-        System.out.println("Enter array elements : ");
-        int twoD[][] = new int[rows][columns];
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-                twoD[i][j] = sc.nextInt();
-            }
-        }
-        System.out.print("\n Data you entered : \n");
-        for (int[] x : twoD) {
-            for (int y : x) {
-
-                System.out.print(y + "        ");
-            }
-            System.out.println();
-        }
+		Sum();
     }
 }
