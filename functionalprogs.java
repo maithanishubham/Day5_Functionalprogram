f112837537cd139b68a1bb23e7d74a871945ba3d
shubham maithani <shubhammaithani1@gmail.com>
2022-07-27 12:19:28 -0700

    2d array

--- functionalprogs.java
+++ functionalprogs.java
@@ -1,10 +1,28 @@
 package FunctionalProgram;
-
+import java.util.Scanner;
 public class functionalprogs {
 
 	public static void main(String[] args) {
-		System.out.println("Functional Programs");
+		
+		Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows : ");
+        int rows = sc.nextInt();
+        System.out.print("Enter the number of columns : ");
+        int columns = sc.nextInt();
+        System.out.println("Enter array elements : ");
+        int twoD[][] = new int[rows][columns];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                twoD[i][j] = sc.nextInt();
+            }
+        }
+        System.out.print("\n Data you entered : \n");
+        for (int[] x : twoD) {
+            for (int y : x) {
 
-	}
-
+                System.out.print(y + "        ");
+            }
+            System.out.println();
+        }
+    }
 }
